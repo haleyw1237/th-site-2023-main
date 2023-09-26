2e8d3210124ae41b4477928d029d2dd818f942d7
Andrew Kim <andrewhkim3@gmail.com>
2023-09-26 17:41:21 -0500

    Change FAQ

---
+++ faq.js
@@ -0,0 +1,7 @@
+const faqCards = document.querySelectorAll(".faq-card");
+
+faqCards.forEach((card) => {
+    card.addEventListener("click", () => {
+        card.classList.toggle("flipped");
+    });
+});
